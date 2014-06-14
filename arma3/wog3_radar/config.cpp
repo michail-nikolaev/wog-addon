635bbb27bcfa70e3c6e48063c6b0715667cf3809
Michail Nikolaev <michail.nikolaev@gmail.com>
2014-06-14 17:45:06 +0300

    gephard support

--- arma3/wog3_radar/config.cpp
+++ arma3/wog3_radar/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.0;
-		requiredAddons[] = {"A3_armor_f_beta_APC_Tracked_02"};
+		requiredAddons[] = {"A3_armor_f_beta_APC_Tracked_02", "A3_Armor_F_Panther"};
 		author[] = {"[TF]Nkey"};
 		authorUrl = "https://github.com/michail-nikolaev/wog-addon";
 		version = 0.0.1;
@@ -19,6 +19,12 @@ class CfgVehicles
 	class O_APC_Tracked_02_base_F;
 	class O_APC_Tracked_02_AA_F : O_APC_Tracked_02_base_F
 	{
+		irscanrangemax = 8000;
+		irscantoeyefactor = 3;
+	};
+	
+	class B_APC_Tracked_01_base_F;
+	class B_APC_Tracked_01_AA_F : B_APC_Tracked_01_base_F {
 		irscanrangemax = 8000;
 		irscantoeyefactor = 3;
 	};
